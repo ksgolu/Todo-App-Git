4318a711690121f5549592efb04409c74dcbbb88
ksgolu <satyamshay@gmail.com>
2019-02-07 12:24:55 +0530

    Add POST/user route and token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,60 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const _ = require('lodash');
+const jwt = require('jsonwebtoken');
 
-const User = mongoose.model('user',{
+mongoose.set('useCreateIndex', true);
+
+const UserSchema = new mongoose.Schema({
     email:  {
         type:     String,
         require:  true,
         trim:     true,
         minlength:  1,
-    }
+        unique:     true,
+        validate: validator.isEmail,
+    },
+    password:{
+        type:       String,
+        minlength:  6,
+        require:    true,
+    },
+    tokens:[{
+        access:{
+            type:   String,
+            require: true,
+        },
+        token:{
+            type: String,
+            require: true,
+        }
+    }]
 });
 
-module.exports = {User};
+//this below function work as an internal and return on pick value. the pick value is catched by
+//header in server.js file
+UserSchema.methods.toJSON = function(){
+    let user = this;
+    let userObject = user.toObject();
+    //console.log(userObject);
+    return _.pick(userObject, ['_id','email']);
+}
+
+UserSchema.methods.generateAuthToken = function(){
+    
+    let user = this;
+    let access = 'auth';
+    let token = jwt.sign({
+        _id: (user._id).toHexString(),
+        access: access,
+    },'abc123').toString();
+   
+
+    user.tokens.push({access,token});
+
+    return user.save().then(() =>{  //here we are returning a value not a promise.
+        return token;
+    })
+};
+let Users = mongoose.model('users',UserSchema);
+module.exports = {Users};
--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 //library module imports
 const express = require('express');
+const{ObjectID} = require('mongodb');
+const _ = require('lodash');
+
 //const bodyParser = require('body-Parser'); instead of this using express built in middleware
 
 // local module imports
+
 const {mongoose} = require('./db/mongoose.js');
 const {Todo} = require('./models/todo.js'); //impoting Todo, exports by mongoose file
-const {User} = require('./models/user.js');
+const {Users} = require('./models/user.js');
 
 let app = express();
 const port = process.env.PORT || 3000;
@@ -16,7 +20,11 @@ data (sent by client) into javascript object and set it to request (req) object 
 //app.use(bodyParser.json());
 //"body-parser": "^1.18.3", -dependcies for json file
 app.use(express.json()); //using express built-in middleware to pase data into json
-
+/*app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });*/
 app.post('/todos',(req,res) => {
     var todo = new Todo({
         text : req.body.text,
@@ -38,6 +46,74 @@ app.get('/todos',(req,res) => {
     }
 });
 
+app.get('/todos/:id',(req,res) =>{
+    let id = req.params.id;
+    if(!ObjectID.isValid(id))
+        return res.status(404).send();
+
+    Todo.findById(id).then((docs) => {
+        if(!docs)
+            return res.status(404).send();
+        res.send({docs});
+    }).catch((e) =>{
+        res.status(400).send();
+    });
+});
+
+app.delete('/todos/:id' ,(req,res) => {
+    let id = req.params.id;
+    if(!ObjectID.isValid(id))
+        return res.status(404).send();
+    Todo.findByIdAndDelete(id).then((docs) =>{
+       if (!docs)
+            return res.status(404).send();
+        res.send({docs});
+    }).catch((e) =>{
+        res.status(400).send({e});
+    });
+
+});
+app.patch('/todos/:id',(req,res) =>{
+    let id = req.params.id;
+    if(!ObjectID.isValid(id))
+        return res.status(404).send();
+
+    let body = _.pick(req.body, ['text','completed']);
+
+    if( _.isBoolean(body.completed) && body.completed) //body.completed will be surely true or false
+    {        
+        body.completedAt = new Date().getTime();
+         /*As we see body have no completedAt value, if we do so, it will automatically updated in body  
+         now body will look :- let body = _.pick(req.body,['text', 'completed', 'completedAt'])
+         */
+    }
+    else
+    {
+        body.completed =false;
+        body.completedAt =null;
+    }
+    Todo.findByIdAndUpdate(id,{$set:body},{new:true})
+    .then((docs) =>{
+        if(!docs)
+            return res.status(404).send();
+        res.send({docs});
+    }).catch((e) =>{
+        res.status(400).send()
+        });
+});
+
+app.post('/users',(req,res) => {
+    let body = _.pick(req.body,['email','password']);
+    let user = new Users(body);
+    
+    user.save().then(() => { 
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth',token).send(user);
+    }).catch((e) => {
+        res.status(400).send();
+    })
+})
 app.listen(port,() =>{
     console.log(`server is up on port : ${port}`);
 });
